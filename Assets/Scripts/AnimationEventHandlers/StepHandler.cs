cd6cb415f825677119ac27fe266baedf4104f1a7
TheWightOne <nathanielkanalz289@gmail.com>
2021-03-07 15:38:34 -0800

    modified the camera and added the animation events for step particles as the player moves.

---
+++ Assets/Scripts/AnimationEventHandlers/StepHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepHandler : MonoBehaviour
+{
+    [Header("Unity Setup Variables")]
+    [SerializeField]private Transform leftFoot = null;
+    [SerializeField]private Transform rightFoot = null;
+    [SerializeField]private Transform parentTransform = null;
+    [SerializeField]private GameObject stepParticle = null;
+    public void FootStepEvent(string foot){
+        if(foot.Equals("left")){
+            Destroy(Instantiate(stepParticle, leftFoot.position, parentTransform.rotation), 2f);
+        }else{
+            Destroy(Instantiate(stepParticle, rightFoot.position, parentTransform.rotation), 2f);
+        }
+        //Debug.Log("Footstep " + foot);
+    }
+}
