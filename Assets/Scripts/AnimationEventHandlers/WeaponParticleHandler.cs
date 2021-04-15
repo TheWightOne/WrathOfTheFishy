542c89891e890e92c0336e575371f5f723162495
TheWightOne <nathanielkanalz289@gmail.com>
2021-04-15 13:25:08 -0700

    added in the animation events for light attacks 1 and 2

---
+++ Assets/Scripts/AnimationEventHandlers/WeaponParticleHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParticleHandler : MonoBehaviour
+{
+    public List<GameObject> particlesToShow;
+    public void ShowParticles(){
+        foreach(GameObject go in particlesToShow){
+            go.SetActive(true);
+        }
+    }
+
+    public void HideParticles(){
+        foreach(GameObject go in particlesToShow){
+            go.SetActive(false);
+        }
+    }
+}
