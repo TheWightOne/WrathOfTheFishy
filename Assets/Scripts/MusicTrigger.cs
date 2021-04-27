3ef2ee1b1f4274d3bd36013c9df63d16d7adb4ca
TheWightOne <nathanielkanalz289@gmail.com>
2021-04-27 15:46:18 -0700

    added music trigger for ingame switching of music

---
+++ Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioClip clipToPlay;
+    private AudioManager audioManager;
+
+    void Start(){
+        audioManager = AudioManager.instance;
+    }
+    void OnTriggerEnter(Collider other){
+        if(!other.CompareTag("Player")){
+            return;
+        }
+        
+        audioManager.StopPersistentClip("mainTheme");
+        audioManager.PlayClip(new AudioSet(clipToPlay, AudioSet.AudioType.MUSIC), "mainTheme");
+        enabled = false;
+    }
+}
