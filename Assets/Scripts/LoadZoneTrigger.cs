529cf487d5fc88f378dccaf779f5a4e00a1892b4
TheWightOne <nathanielkanalz289@gmail.com>
2021-04-26 17:04:33 -0700

    modified the greybox scene to now include enemies. reformatted several sprites towork with the UI. Added preliminary Health bar to the sample scene. adjusted the credits roll script

---
+++ Assets/Scripts/LoadZoneTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadZoneTrigger : MonoBehaviour
+{
+    [SerializeField]private string sceneName;
+    void OnTriggerEnter(Collider other){
+        if(!other.CompareTag("Player")){
+            return;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/UI/UIBar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour
+{
+    enum BarType{
+        NONE,
+        HEALTH,
+        POWER
+    }
+
+    [SerializeField] private CharacterStats statsToLookAt;
+    [Header("Setup Variables")]
+    [SerializeField] private BarType type;
+    [SerializeField] private Image animatedFill;
+    [SerializeField] private Image realFill;
+
+    private int maxValue;
+    private int minValue = 0;
+    private float currentValue;
+
+
+    void Start(){
+        switch(type){
+            case BarType.NONE:
+            return;
+
+            case BarType.HEALTH:
+            maxValue = statsToLookAt.MaxHealth;
+            currentValue = statsToLookAt.CurrentHealth;
+
+            return;
+
+        }
+        
+        
+    }
+
+    void Update(){
+        switch(type){
+            case BarType.NONE:
+            break;
+
+            case BarType.HEALTH:
+            currentValue = statsToLookAt.CurrentHealth;
+
+            break;
+            case BarType.POWER:
+            
+            break;
+
+        }
+
+        float newValue = currentValue.Map(minValue, maxValue, 0f, 1f);
+
+        animatedFill.fillAmount = newValue;
+        realFill.fillAmount = newValue;
+    }
+}
+
+public static class ExtensionMethods {
+ 
+    public static float Map (this float value, float inputFrom, float inputTo, float outputFrom, float outputTo) {
+        return (value - inputFrom) / (inputTo - inputFrom) * (outputTo - outputFrom) + outputFrom;
+    }
+   
+}
