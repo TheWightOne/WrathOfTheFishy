df5d424bc3b5b20a0133bda5439234037a55e678
DESKTOP-73PLVQP\decla <declan.alexander95@gmail.com>
2021-04-28 23:02:07 -0700

    Made the Intro Cutscene, adding a bed, table, stool, mug, torches inside Starter Hut. Added a cutscene specific model (unarmed and armed). Added script for triggering the Njord cutscene and music when you walk into the top of the mountain.

---
+++ Assets/Scripts/CutScenes/CutSceneEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+    public GameObject TimelineObject;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+        StartCoroutine(FinishCut());
+        TimelineObject.SetActive(true);
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(12);
+        thePlayer.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+
+
+}
