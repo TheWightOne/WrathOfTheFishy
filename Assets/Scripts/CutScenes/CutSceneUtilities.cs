f43fe6d099fd1b83841fd7520be10411ef9c917f
TheWightOne <nathanielkanalz289@gmail.com>
2021-05-13 15:32:24 -0700

    Set up the cutscene music switch event via the cutsceneUtilities script

---
+++ Assets/Scripts/CutScenes/CutSceneUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneUtilities : MonoBehaviour
+{
+    AudioManager audioManager = null;
+    
+    void Start(){
+        audioManager = AudioManager.instance;
+    }
+
+    public void SetMusic(AudioClip clipToPlay){
+        audioManager.StopPersistentClip("mainTheme");
+
+        audioManager.PlayClip(new AudioSet(clipToPlay, AudioSet.AudioType.MUSIC), "mainTheme");
+    }
+}
